618b39078b391c3443faafe73f54d0924d3ac40f
Mehedi Hasan <developerhasan99@gmail.com>
2025-09-06 10:57:26 +0600

    Implemented redirected logic on dashboard

--- src/js/pages/dashboard/dashboard.jsx
+++ src/js/pages/dashboard/dashboard.jsx
@@ -3,12 +3,24 @@ import Overview from "./overview";
 import DashboardGraphs from "./graphs";
 import MetricsOverview from "./metrics-overview";
 import SeoIssuesOpportunities from "./seo-issues-opportunities";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import FalconLoader from "../../components/falcon-loader";
+import { useAudit } from "../../hooks/useAudit";
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+  const { isLoading, data, error } = useAudit();
+
+  useEffect(() => {
+    if (!data.recentAudits || data?.recentAudits?.length > 0) {
+      navigate("/run-audit");
+    }
+  }, []);
+
   return (
     <>
-      {true ? (
+      {isLoading ? (
         <div className="h-[calc(100vh-150px)] flex items-center justify-center">
           <FalconLoader />
         </div>
